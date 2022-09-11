2ff0401313aa00c3000b61483c6f3f1afa5b8092
‘Holyfan’ <285184481@qq.com>
2022-09-11 20:45:16 +0800

    hello commit

--- src/main/java/com/tommyhou/springboot/SpringbootApplication.java
+++ src/main/java/com/tommyhou/springboot/SpringbootApplication.java
@@ -1,13 +1,19 @@
 package com.tommyhou.springboot;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+	}
 }
